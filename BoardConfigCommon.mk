87e8ce0ac1487ebf30d5be9a3838949c5eb8a474
thinhx2 <nthinh1102@gmail.com>
2020-02-03 17:49:13 +0700

    set TARGET_TAP_TO_WAKE_NODE to enable d2w

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -60,7 +60,7 @@ TARGET_USES_HWC2 := true
 TARGET_ENABLE_MEDIADRM_64 := true
 
 # Power
-#TARGET_TAP_TO_WAKE_EVENT_NODE := "/dev/input/event2"
+TARGET_TAP_TO_WAKE_NODE := /dev/mokee.touch@1.0/dt2w
 
 # Partitions
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
